b03dc5a079c56129c3d49f47a9ec9ce3b12ab1f4
rayhankinan <rayhankinan@gmail.com>
2023-03-12 18:38:52 +0700

    Initialize file

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+const path = require("path");
+
+module.exports = {
+  mode: "development",
+  entry: "./src/index.ts",
+  devtool: "inline-source-map",
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: "ts-loader",
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".js", ".jsx", ".ts", ".tsx"],
+    alias: {
+      Main: path.resolve(__dirname, "src/"),
+    },
+  },
+  output: {
+    filename: "index.js",
+    path: path.resolve(__dirname, "public/dist"),
+  },
+};

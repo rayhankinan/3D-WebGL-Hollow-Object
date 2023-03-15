3e05345b7d0e2873ca0dc42ae1b2911bd32380cf
rayhankinan <rayhankinan@gmail.com>
2023-03-15 10:56:39 +0700

    Add new model

--- src/index.ts
+++ src/index.ts
@@ -66,6 +66,171 @@ let object: Shape = new Shape(
       ],
       new Color(200, 70, 120)
     ),
+    new Face(
+      [
+        new Point(30, 0, 0),
+        new Point(30, 30, 0),
+        new Point(100, 0, 0),
+        new Point(30, 30, 0),
+        new Point(100, 30, 0),
+        new Point(100, 0, 0),
+      ],
+      new Color(200, 70, 120)
+    ),
+    new Face(
+      [
+        new Point(30, 60, 0),
+        new Point(30, 90, 0),
+        new Point(67, 60, 0),
+        new Point(30, 90, 0),
+        new Point(67, 90, 0),
+        new Point(67, 60, 0),
+      ],
+      new Color(200, 70, 120)
+    ),
+    new Face(
+      [
+        new Point(0, 0, 30),
+        new Point(30, 0, 30),
+        new Point(0, 150, 30),
+        new Point(0, 150, 30),
+        new Point(30, 0, 30),
+        new Point(30, 150, 30),
+      ],
+      new Color(80, 70, 200)
+    ),
+    new Face(
+      [
+        new Point(30, 0, 30),
+        new Point(100, 0, 30),
+        new Point(30, 30, 30),
+        new Point(30, 30, 30),
+        new Point(100, 0, 30),
+        new Point(100, 30, 30),
+      ],
+      new Color(80, 70, 200)
+    ),
+    new Face(
+      [
+        new Point(30, 60, 30),
+        new Point(67, 60, 30),
+        new Point(30, 90, 30),
+        new Point(30, 90, 30),
+        new Point(67, 60, 30),
+        new Point(67, 90, 30),
+      ],
+      new Color(80, 70, 200)
+    ),
+    new Face(
+      [
+        new Point(0, 0, 0),
+        new Point(100, 0, 0),
+        new Point(100, 0, 30),
+        new Point(0, 0, 0),
+        new Point(100, 0, 30),
+        new Point(0, 0, 30),
+      ],
+      new Color(70, 200, 210)
+    ),
+    new Face(
+      [
+        new Point(100, 0, 0),
+        new Point(100, 30, 0),
+        new Point(100, 30, 30),
+        new Point(100, 0, 0),
+        new Point(100, 30, 30),
+        new Point(100, 0, 30),
+      ],
+      new Color(200, 200, 70)
+    ),
+    new Face(
+      [
+        new Point(30, 30, 0),
+        new Point(30, 30, 30),
+        new Point(100, 30, 30),
+        new Point(30, 30, 0),
+        new Point(100, 30, 30),
+        new Point(100, 30, 0),
+      ],
+      new Color(210, 100, 70)
+    ),
+    new Face(
+      [
+        new Point(30, 30, 0),
+        new Point(30, 60, 30),
+        new Point(30, 30, 30),
+        new Point(30, 30, 0),
+        new Point(30, 60, 0),
+        new Point(30, 60, 30),
+      ],
+      new Color(210, 160, 70)
+    ),
+    new Face(
+      [
+        new Point(30, 60, 0),
+        new Point(67, 60, 30),
+        new Point(30, 60, 30),
+        new Point(30, 60, 0),
+        new Point(67, 60, 0),
+        new Point(67, 60, 30),
+      ],
+      new Color(70, 180, 210)
+    ),
+    new Face(
+      [
+        new Point(67, 60, 0),
+        new Point(67, 90, 30),
+        new Point(67, 60, 30),
+        new Point(67, 60, 0),
+        new Point(67, 90, 0),
+        new Point(67, 90, 30),
+      ],
+      new Color(100, 70, 210)
+    ),
+    new Face(
+      [
+        new Point(30, 90, 0),
+        new Point(30, 90, 30),
+        new Point(67, 90, 30),
+        new Point(30, 90, 0),
+        new Point(67, 90, 30),
+        new Point(67, 90, 0),
+      ],
+      new Color(76, 210, 100)
+    ),
+    new Face(
+      [
+        new Point(30, 90, 0),
+        new Point(30, 150, 30),
+        new Point(30, 90, 30),
+        new Point(30, 90, 0),
+        new Point(30, 150, 0),
+        new Point(30, 150, 30),
+      ],
+      new Color(140, 210, 80)
+    ),
+    new Face(
+      [
+        new Point(0, 150, 0),
+        new Point(0, 150, 30),
+        new Point(30, 150, 30),
+        new Point(0, 150, 0),
+        new Point(30, 150, 30),
+        new Point(30, 150, 0),
+      ],
+      new Color(90, 130, 110)
+    ),
+    new Face(
+      [
+        new Point(0, 0, 0),
+        new Point(0, 0, 30),
+        new Point(0, 150, 30),
+        new Point(0, 0, 0),
+        new Point(0, 150, 30),
+        new Point(0, 150, 0),
+      ],
+      new Color(160, 160, 220)
+    ),
   ],
   100,
   100,

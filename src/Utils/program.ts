64a77839e218f2235c64166c0d6bf253cea60f67
rayhankinan <rayhankinan@gmail.com>
2023-03-24 11:31:58 +0700

    Minor changes to structure

--- src/Utils/program.ts
+++ src/Utils/program.ts
@@ -8,11 +8,6 @@ function createProgram(
   gl.attachShader(program, fragmentShader);
   gl.linkProgram(program);
 
-  const ext = gl.getExtension("OES_element_index_uint");
-  if (!ext) {
-    throw Error("OES_element_index_uint is not supported!");
-  }
-
   const success = gl.getProgramParameter(program, gl.LINK_STATUS) as boolean;
   if (!success) {
     gl.deleteProgram(program);
